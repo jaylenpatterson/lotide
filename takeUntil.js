0b2654eed1617a870622e7fb44fc089b992c4bc3
jaylenpatterson <jaylenpatterson16@gmail.com>
2021-11-25 20:12:00 +0000

    created a function that takes away values until the determined value is met.

---
+++ takeUntil.js
@@ -0,0 +1,27 @@
+const takeUntil = function (array, callback) {
+  let newArr = []; 
+
+  for(element of array){
+    if(callback(element)){
+      return newArr
+    } else {
+      newArr.push(element)
+    }
+  }
+return newArr
+}
+
+  
+
+
+/*(const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);*/
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1)
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
