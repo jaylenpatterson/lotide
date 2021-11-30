962bf6a45def3140601e0eeb5078f9a7c491da7a
jaylenpatterson <jaylenpatterson16@gmail.com>
2021-11-30 18:32:50 +0000

    linting & more testing

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
-const map = function (arr, callback ) {
+// creates a new array, executes the callback function on each item in the original array then sends the results to the new array, returns the new array
+
+const map = function(arr, callback) {
   let newArr = [];
-  for(item of arr) {
+  for (let item of arr) {
     newArr.push(callback(item));
   }
   return newArr;
-}
-
-console.log(map([1,2,3,4], x => x + 1 ));
+};
 
 module.exports = map;
 
--- test/mapTest.js
+++ test/mapTest.js
@@ -1,7 +1,7 @@
 const map = require('../map');
 const assert = require('chai').assert;
 
-describe("#assertEqual()", () => {
+describe("#map()", () => {
   it(`should return [7, 10, 6, 5]`, () => {
     assert.deepEqual(map([4, 7, 3, 2], x => x + 3), [7, 10, 6, 5]);
   });
@@ -9,7 +9,18 @@ describe("#assertEqual()", () => {
   it(`should return [4, 6, 8, 10]`, () => {
     assert.deepEqual(map([2, 4, 6, 8], x => x + 2), [4, 6, 8, 10]);
   });
+
+  it(`should return [2, 4, 6, 8]`, () => {
+    assert.deepEqual(map([2, 4, 6, 8], x => x + 0), [2, 4, 6, 8]);
+  });
+
+  it(`should return [4, 16, 36, 64]`, () => {
+    assert.deepEqual(map([2, 4, 6, 8], x => x * x), [4, 16, 36, 64]);
+  });
+
+  it(`should return [2, 4, 6, 8]`, () => {
+    assert.deepEqual(map([2, 4, 6, 8], x => x), [2, 4, 6, 8]);
+  });
 });
 
-const words = ["ground", "control", "to", "major", "tom"];
 
